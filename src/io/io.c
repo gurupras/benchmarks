aecfa70836eb4fbf4234c90f2ccecbc88c9cf0f7
Guru Prasad <gurupras@buffalo.edu>
2013-07-29 11:24:35 -0400

    Cleaned IO benchmark

--- src/io/io.c
+++ src/io/io.c
@@ -11,22 +11,6 @@ static struct list *pids;
 
 
 static int run_benchmark(int argc, char **argv) {
-	init_list(pids);
-
-	int i;
-
-	for(i = 0; i < 5; i++) {
-		append(pids, i);
-		printf("list->last_ptr = %X\n", pids->last_ptr);
-	}
-
-	int index = 0;
-	int *entry;
-	for_each_entry(entry, pids) {
-		printf("list->cur_ptr  = %X\n", __node);
-		printf("list[%d] = %d\n", index, *entry);
-		index++;
-	}
 }
 
 struct benchmark io = {
